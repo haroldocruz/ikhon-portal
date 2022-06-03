7ca2c99c5da028ed08b9216542299cc8fdc2a904
Haroldo Cruz <haroldocruzdr@gmail.com>
2022-06-03 11:55:37 -0300

    feature(layout): layout principal desestruturado em componentes

---
+++ src/app/core/layout/customizer/customizer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customizer',
+  templateUrl: './customizer.component.html',
+  styleUrls: ['./customizer.component.less']
+})
+export class CustomizerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
